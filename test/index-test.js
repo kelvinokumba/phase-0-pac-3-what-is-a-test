7af15372323ebfb89b6772c811659a1aea82cd37
Kelvin okumba <okumbakelvin@gmail.com>
2023-07-11 23:28:23 +0300

    completed assignment

--- test/index-test.js
+++ test/index-test.js
@@ -1,12 +1,11 @@
 const { name, height, message } = require("../index.js");
 
-/*
 describe('what-is-a-test', () => {
   describe('Name', () => {
     it('returns "Susan"', () => {
-      expect(name).toEqual('Susan')
-    })
-  })
+      expect(name).toEqual('Joe')
+    });
+  });
 
 
   describe('Height', () => {
@@ -22,4 +21,3 @@ describe('what-is-a-test', () => {
     })
   })
 })
-*/
